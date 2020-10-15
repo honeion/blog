386025d383f75165a6c140ea0311ab2dc35d0ef0
honeion <honeion2012@gmail.com>
2020-10-15 15:18:11 +0900

    express start

---
+++ server/app.js
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+
+//모든 신호 받음
+app.get('/')
+
+export default app;
---
+++ server/server.js
@@ -0,0 +1,5 @@
+import app from './app'
+//7000포트를 받고 있고
+app.listen('7000',() => {
+    console.log('hi');
+});
