e1e06bb63d71ec44e7806f2de189bb50543c849d
honeion <honeion2012@gmail.com>
2020-11-10 16:05:32 +0900

    add routes

---
+++ client/src/routes/normalRoute/PostDetail.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PostDetail = () => {
+    //일단 확인 할 수 있도록
+    return <h1>PostDetail</h1>;
+}
+
+export default PostDetail;
---
+++ client/src/routes/normalRoute/PostWrite.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const PostWrite = () => {
+    //일단 확인 할 수 있도록
+    return <h1>PostWrite</h1>;
+}
+
+export default PostWrite;
