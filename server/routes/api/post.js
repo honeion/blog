93b26bd50100935fae123200bb15c707d1657689
honeion <honeion2012@gmail.com>
2020-11-12 22:52:51 +0900

    server - update post.js for saving img to aws-s3, multer-s3 option haven't aws region. i don't understand req.files.map() error

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -6,24 +6,61 @@ import Post from '../../models/post'
 
 const router = express.Router()
 
+import multer from 'multer'
+import multerS3 from 'multer-s3'
+import path from 'path'
+import AWS from 'aws-sdk'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_PRIVATE_KEY
+})
+const uploadS3 = multer({
+    storage: multerS3({
+        s3,
+        bucket: "s3-myblog/upload",
+        key(req, file, cb) {
+            const ext = path.extname(file.originalname)
+            const basename = path.basename(file.originalname, ext)
+            cb(null, basename + new Date().valueOf() + ext) //callback
+        }
+    }), limits: { fileSize: 100 * 1024 * 1024 }, //100mb
+})
+// region: "ap-northeast-2",
+
+// @route   POST api/post/image
+// @desc    Create a Post
+// @access  Private
+router.post("/image", uploadS3.array("upload", 5), async (req, res, next) => { //next : 에러가 나면 다음으로 넘김
+    try {
+        console.log(req.files.map((v) => v.loaction))
+        res.json({ uploaded: true, url: req.files.map((v) => v.loaction) })
+    } catch (e) {
+        console.error(e)
+        res.json({ uploaded: false, url: null })
+    }
+})
+
 // api/post
-router.get('/' , async(req, res)=> {
+router.get('/', async (req, res) => {
     const postFindResult = await Post.find(); //몽고디비에서 모든 것 가져옴
     console.log(postFindResult, "ALL Post Get");
     res.json(postFindResult) //마지막 줄에 응답이 나가야함
 })
 
-router.post('/' , auth , async(req, res) => {
-    try{
+router.post('/', auth, async (req, res) => {
+    try {
         console.log(req, "req");
-        const { title , contents, fileUrl, creator } = req.body;
+        const { title, contents, fileUrl, creator } = req.body;
         //req.body.title, req.body.contents ...
         const newPost = await Post.create({
-            title : title, 
+            title: title,
             contents, fileUrl, creator // 똑같으면 생략도 가능
         }); //async await 안쓰고 .exec() 붙여서 실행해도 가능
         res.json(newPost);
-    } catch(e){
+    } catch (e) {
         console.log(e);
     }
 })
