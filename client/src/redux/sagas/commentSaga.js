12f84a9479a9314ed39365a789c8f2cb9f5898f0
honeion <honeion2012@gmail.com>
2020-12-07 18:23:10 +0900

    write comment saga

--- client/src/redux/sagas/commentSaga.js
+++ client/src/redux/sagas/commentSaga.js
@@ -1,1 +1,70 @@
 //server router에서 sagas와 신호를 주고 받아야함
+import axios from 'axios';
+import { all, call, fork, put, takeEvery } from "redux-saga/effects";
+import { COMMENT_LOADING_FAILURE, COMMENT_LOADING_REQUEST, COMMENT_LOADING_SUCCESS, COMMENT_UPLOADING_FAILURE, COMMENT_UPLOADING_REQUEST, COMMENT_UPLOADING_SUCCESS } from "../types";
+import { push } from 'connected-react-router'
+// Load Comment
+
+const loadCommentsAPI = (payload) => {
+    console.log(payload, "loadCommentAPI ID")
+    //payload = post id
+    return axios.get(`/api/post/${payload}/comments`)
+}
+
+function* loadComments(action){
+    try {
+        const result = yield call(loadCommentsAPI, action.payload)
+        console.log(result)
+        yield put({
+            type:COMMENT_LOADING_SUCCESS,
+            payload : result.data,
+        })
+    } catch (error) {
+        console.log(error)
+        yield put({
+            type: COMMENT_LOADING_FAILURE,
+            payload : error
+        })
+        yield put(push('/'))
+    }
+}
+
+function* watchLoadComments(){
+    yield takeEvery(COMMENT_LOADING_REQUEST, loadComments); //loading request를 매번 감시하다 있으면 loadComments를 불러옴
+}
+
+// Upload Comment
+
+const uploadCommentsAPI = (payload) => {
+    console.log(payload.id, "uploadCommentsAPI ID")
+    //upload시에는 넘겨오는 데이터가 댓글관련된 폼 내용이니 id만 추출
+    return axios.post(`/api/post/${payload.id}/comments`)
+}
+
+function* uploadComments(action){
+    try {
+        const result = yield call(uploadCommentsAPI, action.payload)
+        console.log(result, "Upload Comment")
+        yield put({
+            type:COMMENT_UPLOADING_SUCCESS,
+            payload : result.data,
+        })
+    } catch (error) {
+        console.log(error)
+        yield put({
+            type: COMMENT_UPLOADING_FAILURE,
+            payload : error
+        })
+        yield put(push('/'))
+    }
+}
+
+function* watchUploadComments(){
+    yield takeEvery(COMMENT_UPLOADING_REQUEST, uploadComments); 
+}
+export default function* commentSaga() {
+    yield all([
+        fork(watchLoadComments),
+        fork(watchUploadComments),
+    ])
+}
--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -4,13 +4,15 @@ import axios from 'axios'
 import authSaga from './authSaga'
 import dotenv from 'dotenv'
 import postSaga from './postSaga'
+import commentSaga from './commentSaga'
 dotenv.config()
 axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL
 
 export default function* rootSaga() {
     yield all([
         fork(authSaga), //찍어서 불러옴
-        fork(postSaga)
+        fork(postSaga),
+        fork(commentSaga),
     ]);
 }
 //function* 이 함수는 generator 함수(여러값을 반환할 수 있음)
